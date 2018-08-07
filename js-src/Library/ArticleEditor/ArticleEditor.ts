c31b14ddc52903af22e9350635b6d636f9b5c454
Mikey Clarke <mikey@tryhleo.com>
2018-08-07 23:11:35 +0100

    Add highlight plugin

--- js-src/Library/ArticleEditor/ArticleEditor.ts
+++ js-src/Library/ArticleEditor/ArticleEditor.ts
@@ -1,13 +1,15 @@
 import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 
 class ArticleEditor {
     private ckEditorInstance: BalloonEditor;
@@ -22,8 +24,10 @@ class ArticleEditor {
             Link,
             List,
             Paragraph,
+            Strikethrough,
+            Highlight,
         ],
-        toolbar: ['bold', 'italic', 'strikethrough', 'blockQuote'],
+        toolbar: ['bold', 'italic', 'strikethrough', 'blockQuote', 'link'],
     };
 
     constructor(element: HTMLElement) {

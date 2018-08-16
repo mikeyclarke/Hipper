38782cf25b0fafa2a2297e2818699b264b12dcc1
Mikey Clarke <mikey@tryhleo.com>
2018-08-16 01:07:05 +0100

    PostgreSQL column names are lower cased

--- sql/init.sql
+++ sql/init.sql
@@ -8,15 +8,15 @@ CREATE TABLE organization (
 CREATE TYPE organization_roles AS ENUM ('owner', 'admin', 'member');
 
 CREATE TABLE person (
-    id                  varchar(15) NOT NULL primary key,
-    name                varchar(255) NOT NULL,
-    emailAddress        varchar(255) NOT NULL,
-    password            varchar(150) NOT NULL,
-    role                organization_roles DEFAULT 'member',
-    emailVerified       boolean DEFAULT false,
-    organizationId      varchar(15) NOT NULL references organization(id),
-    created             timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated             timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id                      varchar(15) NOT NULL primary key,
+    name                    varchar(255) NOT NULL,
+    email_address           varchar(255) NOT NULL,
+    password                varchar(150) NOT NULL,
+    role                    organization_roles DEFAULT 'member',
+    email_address_verified  boolean DEFAULT false,
+    organization_id         varchar(15) NOT NULL references organization(id),
+    created                 timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated                 timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE OR REPLACE FUNCTION update_updated_timestamp()

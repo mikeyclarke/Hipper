4170570d78ec233b444b3938b1bacf69853e7391
Mikey Clarke <mikey@tryhleo.com>
2018-08-16 22:33:31 +0100

    Improved UUID creation

--- src/php/IdGenerator/IdGenerator.php
+++ src/php/IdGenerator/IdGenerator.php
@@ -2,12 +2,30 @@
 namespace hleo\IdGenerator;
 
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
+use Ramsey\Uuid\Generator\CombGenerator;
+use Ramsey\Uuid\Codec\TimestampFirstCombCodec;
 
 class IdGenerator
 {
+    private $factory;
+
+    public function __construct()
+    {
+        $this->factory = new UuidFactory;
+
+        $generator = new CombGenerator(
+            $this->factory->getRandomGenerator(),
+            $this->factory->getNumberConverter()
+        );
+        $codec = new TimestampFirstCombCodec($this->factory->getUuidBuilder());
+
+        $this->factory->setRandomGenerator($generator);
+        $this->factory->setCodec($codec);
+    }
+
     public function generate()
     {
-        $id = Uuid::uuid1();
-        return $id->toString();
+        return $this->factory->uuid4()->toString();
     }
 }

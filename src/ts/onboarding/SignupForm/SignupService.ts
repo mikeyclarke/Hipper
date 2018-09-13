d09e7a34a6b5e739306b4b57da03179d37fce4b7
Mikey Clarke <mikey@tryhleo.com>
2018-09-13 05:38:08 +0100

    Fix API request

--- src/ts/onboarding/SignupForm/SignupService.ts
+++ src/ts/onboarding/SignupForm/SignupService.ts
@@ -3,9 +3,6 @@ class SignupService
     public static submitForm(callback, payload) {
         return fetch('/_/sign-up', {
             method: "POST",
-            headers: {
-                "Content-Type": "Content-Type: multipart/form-data;",
-            },
             body: payload,
         })
         .then((response) => {

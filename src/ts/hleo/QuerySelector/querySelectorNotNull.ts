d32e14b9fdfc987c0a589c64c90871a7f01e9131
Matt Arnold <matthew.arnold@basekit.com>
2019-03-05 23:05:40 +0000

    Remove use of any in web component

--- src/ts/hleo/QuerySelector/querySelectorNotNull.ts
+++ src/ts/hleo/QuerySelector/querySelectorNotNull.ts
@@ -1,4 +1,6 @@
-export function querySelectorNotNull(context: any, selector: string): HTMLElement {
+import { DOMQueryable } from './DOMQueryable';
+
+export function querySelectorNotNull(context: DOMQueryable, selector: string): HTMLElement {
     const result = context.querySelector(selector);
     if (result) {
         return result;

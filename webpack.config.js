35d241c207cccb854355a96fce3d4c440ef72d1b
Mikey Clarke <mikey@tryhleo.com>
2019-08-11 19:46:41 +0100

    Update npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 

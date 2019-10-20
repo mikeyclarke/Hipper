7abc90e2f52dfd5e5d4a48c131889a306527ce4d
Mikey Clarke <mikey@usehipper.com>
2019-10-20 23:03:15 +0100

    Add the ability to sign out

---
+++ src/php/FrontEnd/App/Controller/Organization/SignOutController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Hipper\FrontEnd\App\Controller\Organization;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class SignOutController
+{
+    public function signOut(Request $request): RedirectResponse
+    {
+        $session = $request->getSession();
+        $session->invalidate();
+
+        return new RedirectResponse('/sign-in');
+    }
+}

6e1964a550625046f1fe2dc648450ee21c6c2eb1
Mikey Clarke <mikey@usehipper.com>
2020-03-02 13:39:16 +0000

    Allow onboarding to be bypassed

--- src/php/FrontEnd/App/Middleware/Organization/OnboardingCompletedMiddleware.php
+++ src/php/FrontEnd/App/Middleware/Organization/OnboardingCompletedMiddleware.php
@@ -3,16 +3,36 @@ declare(strict_types=1);
 
 namespace Hipper\FrontEnd\App\Middleware\Organization;
 
+use Hipper\Person\Storage\PersonUpdater as PersonStorageUpdater;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class OnboardingCompletedMiddleware
 {
+    private const COMPLETE_URL_SEARCH_PARAM = 'explorer';
+
+    private PersonStorageUpdater $personStorageUpdater;
+
+    public function __construct(
+        PersonStorageUpdater $personStorageUpdater
+    ) {
+        $this->personStorageUpdater = $personStorageUpdater;
+    }
+
     public function before(Request $request)
     {
         $currentUser = $request->attributes->get('current_user');
-        if (!$currentUser->isOnboardingCompleted()) {
-            return new RedirectResponse('/welcome');
+        $completeOnboarding = $request->query->getBoolean(self::COMPLETE_URL_SEARCH_PARAM);
+
+        if ($currentUser->isOnboardingCompleted()) {
+            return;
         }
+
+        if ($completeOnboarding) {
+            $this->personStorageUpdater->update($currentUser->getId(), ['onboarding_completed' => true]);
+            return new RedirectResponse('/');
+        }
+
+        return new RedirectResponse('/welcome');
     }
 }
